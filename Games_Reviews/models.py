9b3a2ee7698e547865ea333a4662e0e45e2b80ea
Everton-Renan <evertonrenan917@gmail.com>
2024-10-30 11:59:35 -0300

    Create models Review, Genre and Mode

--- Games_Reviews/models.py
+++ Games_Reviews/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    title = models.CharField()
+    reviews = models.PositiveIntegerField()
+    developer = models.CharField()
+    publisher = models.CharField()
+    series = models.CharField()
+    platforms = models.CharField()
+    release = models.DateField()
+
+
+class Genre(models.Model):
+    genre = models.CharField(max_length=50)
+    review = models.ForeignKey(
+        Review, on_delete=models.SET_NULL
+    )
+
+    def __str__(self) -> str:
+        return self.genre
+
+
+class Mode(models.Model):
+    mode = models.CharField(max_length=50)
+    review = models.ForeignKey(
+        Review, on_delete=models.SET_NULL
+    )
+
+    def __str__(self) -> str:
+        return self.mode
