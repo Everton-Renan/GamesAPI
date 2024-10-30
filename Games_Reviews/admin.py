2554b7c94f97d9cef39a4b6cfab734a7e79897b2
Everton-Renan <evertonrenan917@gmail.com>
2024-10-30 12:05:05 -0300

    Create GenreInLine, ModeInLine and ReviewsAdmin

--- Games_Reviews/admin.py
+++ Games_Reviews/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from Games_Reviews import models
+
+
+class GenreInLine(admin.TabularInline):
+    model = models.Genre
+    extra = 1
+
+
+class ModeInLine(admin.TabularInline):
+    model = models.Mode
+    extra = 1
+
+
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'reviews', 'release', 'publisher')
+    inlines = GenreInLine, ModeInLine
