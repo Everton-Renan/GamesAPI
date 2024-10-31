d66b0074a3c41bcdbc0c9b7fd60402141d2361b3
Everton-Renan <evertonrenan917@gmail.com>
2024-10-31 20:11:38 -0300

    Create home url

--- Games_Reviews/urls.py
+++ Games_Reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from Games_Reviews.views import return_data
+from Games_Reviews.views import home, return_data
 
 urlpatterns = [
-    path('api/<str:username>/<str:key>', return_data, name='return_data')
+    path('api/<str:username>/<str:key>', return_data, name='return_data'),
+    path('', home, name='home')
 ]
