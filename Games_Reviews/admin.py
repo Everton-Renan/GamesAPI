2ffc463e7fd08b3acac7494a6a951ef1d77d71f1
Everton-Renan <evertonrenan917@gmail.com>
2024-10-31 16:00:33 -0300

    Remove GenreInLine and ModeInLine

--- Games_Reviews/admin.py
+++ Games_Reviews/admin.py
@@ -3,21 +3,10 @@ from django.contrib import admin
 from Games_Reviews import models
 
 
-class GenreInLine(admin.TabularInline):
-    model = models.Genre
-    extra = 1
-
-
-class ModeInLine(admin.TabularInline):
-    model = models.Mode
-    extra = 1
-
-
 @admin.register(models.Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'reviews', 'release', 'publisher')
     list_display_links = ('id', 'title')
-    inlines = GenreInLine, ModeInLine
 
 
 @admin.register(models.NewUser)
