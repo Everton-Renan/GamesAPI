7cd11b055a8f03bcdad45df1a098b40ee1d8368d
Everton-Renan <evertonrenan917@gmail.com>
2024-10-31 16:33:05 -0300

    Create return_data

--- Games_Reviews/views.py
+++ Games_Reviews/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, JsonResponse
 
-# Create your views here.
+from Games_Reviews.models import NewUser, Review
+
+
+def return_data(request: HttpRequest, username: str, key: str):
+    if request.method == 'POST':
+        return HttpResponse('POST method is not accepted.')
+    user = NewUser.objects.filter(username=username)
+    if key != user.get().key:
+        return HttpResponse("Key errada.")
+    games = Review.objects.all().values(
+        'title',
+        'reviews',
+        'image',
+        'developer',
+        'publisher',
+        'series',
+        'platforms',
+        'release',
+        'genre',
+        'mode'
+    )
+
+    games_list = list(games)
+    return JsonResponse(games_list, safe=False)
