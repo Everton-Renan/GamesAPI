1d6f35271be47c602fbccdc2aa01c0584b749dd9
Everton-Renan <evertonrenan917@gmail.com>
2024-11-01 12:33:29 -0300

    Set the app_name and create the single_game url

--- Games_Reviews/urls.py
+++ Games_Reviews/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from Games_Reviews.views import home, return_data
+from Games_Reviews.views import home, return_data, single_game
+
+app_name = 'Games_Reviews'
 
 urlpatterns = [
     path('api/<str:username>/<str:key>', return_data, name='return_data'),
+    path('game/<int:_id>', single_game, name='single_game'),
     path('', home, name='home')
 ]
