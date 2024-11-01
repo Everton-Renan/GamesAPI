e19c88b105f4338dadb4f0ebc5d0905dad664e19
Everton-Renan <evertonrenan917@gmail.com>
2024-10-31 23:53:12 -0300

    Configure request method post in home view

--- Games_Reviews/views.py
+++ Games_Reviews/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
 
@@ -28,6 +29,20 @@ def return_data(request: HttpRequest, username: str, key: str):
 
 
 def home(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        search = request.POST.get('query')
+        games = Review.objects.filter(
+            Q(title__contains=search) |
+            Q(platforms__contains=search) |
+            Q(publisher__contains=search) |
+            Q(reviews__contains=search) |
+            Q(release__contains=search)
+        )
+        context = {
+            'games': games
+        }
+        return render(request, 'index.html', context)
+
     games = Review.objects.all()
     context = {
         'games': games
